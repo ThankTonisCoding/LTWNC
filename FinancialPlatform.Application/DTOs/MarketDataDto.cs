c97f40f8bc2d4a7cea65c971ddb44c6bd022830d
ThankTonisCoding <thanhtoancris2004@gmail.com>
2026-03-15 02:14:33 +0700

    Update 153

--- FinancialPlatform.Application/DTOs/MarketDataDto.cs
+++ FinancialPlatform.Application/DTOs/MarketDataDto.cs
@@ -4,9 +4,10 @@ namespace FinancialPlatform.Application.DTOs
 {
     public class MarketDataDto
     {
-        public string Symbol { get; set; }
-        public decimal Price { get; set; }
-        public string TimeFormatted { get; set; } // Dạng string để hiển thị UI dễ hơn
-        public string Color { get; set; } // "green" hoặc "red" để UI hiển thị
+        public required string Symbol { get; set; }
+        public required string TimeFormatted { get; set; }
+        public required string Color { get; set; }
+
+
     }
 }
